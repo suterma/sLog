29134946963334d0d95dff76abd26b387baffa90
Marcel <marcel+github@codeministry.ch>
2018-12-04 00:08:59 +0100

    added first draft of analyzing view

---
+++ src/sLog.Analyzers/PingLogAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using sLog.Demo.DataSource.NetScanner;
+using sLog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace sLog.Analyzers
+{
+    /// <summary>An analyzer for ping logs.</summary>
+    public class PingLogAnalyzer
+    {
+        //TODO Later Use Log<PingResult>
+
+        public DateTime TimeStamp { get; set; }
+        public string Target { get; set; }
+        public IPStatus Status { get; set; }
+        public TimeSpan RoundtripTime { get; set; }
+
+        public static IQueryable<PingLogAnalyzer> Filter(IQueryable<Log> filterable)
+        {
+            return filterable
+                .Where(log => log.ContentType.Equals("sLog.Demo.DataSource.NetScanner.PingResult"))
+                .Select(log => new PingLogAnalyzer(log));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PingLogAnalyzer"/> class.
+        /// </summary>
+        /// <param name="log">The log.</param>
+        public PingLogAnalyzer(Log log)
+        {
+            TimeStamp = log.Timestamp;
+            PingResult pingResult = JsonConvert.DeserializeObject<PingResult>(log.Data);
+            Target = pingResult.Target;
+            Status = pingResult.Status;
+            RoundtripTime = pingResult.RoundtripTime;
+            
+        }
+
+        public PingLogAnalyzer()
+        {
+        }
+    }
+
+
+}
---
+++ src/sLog/Controllers/AnalyzeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using sLog.Analyzers;
+using sLog.Models;
+
+namespace sLog.Controllers
+{
+    public class AnalyzeController : Controller
+    {
+        private readonly sLogContext _context;
+
+        public AnalyzeController(sLogContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Logs
+        public async Task<IActionResult> Index()
+        {
+            DbSet<Log> logs = _context.Log;
+            IQueryable<PingLogAnalyzer> pingLogs = PingLogAnalyzer.Filter(logs);
+            return View(await pingLogs.ToListAsync());
+        }
+
+        // GET: Logs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            string targetIp = id;
+            if (targetIp == null)
+            {
+                return NotFound();
+            }
+
+            DbSet<Log> logs = _context.Log;
+            IQueryable<PingLogAnalyzer> pingLogs = PingLogAnalyzer
+                .Filter(logs)
+                .Where(pingLog => pingLog.Target == targetIp);
+
+            var allPingLogs = await pingLogs.ToListAsync();
+
+            //TODO use chart instead of this pseudo average
+            var averagedPingLog = new PingLogAnalyzer()
+            {
+                TimeStamp = allPingLogs.FirstOrDefault().TimeStamp,
+                Target = targetIp,
+                Status = 0,
+                RoundtripTime = TimeSpan.FromTicks((long)allPingLogs.Average(ping => ping.RoundtripTime.Ticks))
+
+            };
+            return View(averagedPingLog);
+        }  
+    }
+}
