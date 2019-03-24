58e15d6b0380e9b5b5fe388a436d978deca8c7b2
Marcel <marcel+github@codeministry.ch>
2019-03-25 00:14:19 +0100

    Added skeleton qr code controller

---
+++ src/sLog/Controllers/QrCodeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sLog.Controllers
+{
+    /// <summary>
+    /// An API Controller, that produces a QR Code Image from route data.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrCodeController : ControllerBase
+    {
+        // GET: api/QrCode/text
+        [HttpGet("{text}")]
+        public async Task<IActionResult> GetQrCode([FromRoute] string text)
+        {
+            //TODO return a proper QR code image
+            //TODO move to the QuickCore assembly and import using Aplication Parts
+            //read https://docs.microsoft.com/en-us/aspnet/core/mvc/advanced/app-parts?view=aspnetcore-2.2
+            return Ok("image//TODO");
+        }
+    }
+}
