4fab8a20a3fbd1e557ed8b28ed09488810bf056f
Marcel <marcel+github@codeministry.ch>
2019-04-01 00:14:55 +0200

    Finished the URL matching for the appendix controller

--- src/sLog/Controllers/QrAppendixController.cs
+++ src/sLog/Controllers/QrAppendixController.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace sLog.Controllers
 {
     /// <summary>An API Controller, that produces a QR Code Image from route data.</summary>
     /// <remarks>
-    ///   <para>Call this API by appending a "/qrcode/size/margin" at the end of an existing URL:    
+    ///   <para>Call this API by appending a "/qrcode/size/margin" at the end (as 'appendix') of an existing URL:    
     ///   <list type="bullet">
     ///     <item>
     /// The text is the URL withouth the "/qrcode/size/margin" part
@@ -23,42 +21,54 @@ namespace sLog.Controllers
     ///   </para>
     /// </remarks>
     /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase"/>
-    [Route("testpathsegment/qrcode")] //TODO see templates for matching */qrcode
+    /// <devdoc>
+    /// <para>To have the end of the URL match (instead of a prefix), the asterisk variant of the regex matcher is used. The regex will match any URL that has a '/qrcode' segement, followed by any number of digit-only segments (the optional QR code parameters). See https://docs.microsoft.com/en-us/aspnet/web-api/overview/web-api-routing-and-actions/create-a-rest-api-with-attribute-routing#get-books-by-publication-date with the wildcard example at the end.</para>
+    /// </devdoc>
+    [Route("/{*url:regex(^.*\\/qrcode(\\/\\d*)*$)}")]
     [ApiController]
     public class QrAppendixController : ControllerBase
     {
         /// <summary>
-        /// Gets the QR code according to the existing route data.
+        /// Gets the QR code for the given matched URL.
         /// </summary>
-        /// <param name="size">The size (width x height), in (screen) pixels. Optional, default ist 100.</param>
-        /// <param name="margin">The margin (quiet zone), in QR pixels. Optional, default ist 5.</param>
+        /// <remarks>
+        /// The default size = 100, the default margin = 5
+        /// </remarks>
+        /// <param name="url">The URL's path from the request.</param>
         /// <returns></returns>
-        [HttpGet("{size:int?}/{margin:int?}")]
-        public async Task<IActionResult> GetQrCode([FromRoute] int? size = 100, [FromRoute] int? margin = 5)
+        /// <devdoc>The parameters can not be used, since the URL matcher already consumed the complete URL. We must get the QR parameters from the (relative) URL</devdoc>
+        [HttpGet()]
+        public async Task<IActionResult> GetQrCode([FromRoute] string url)
         {
-            //TODO move to the QuickCore assembly and import using Aplication Parts
-            //Get the intended route as text
-            //TODO remove the qr addons
-            
-            string targetUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}{HttpContext.Request.Path}{HttpContext.Request.QueryString}";
-            var text = targetUrl;
+            //TODO move to the QuickCore assembly and import using Application Parts
+            var qrPostfix = url.Substring(url.IndexOf("/qrcode"));
+            var qrSegments = qrPostfix.Split('/');
+
+            int size;
+            if (!int.TryParse(qrSegments.Skip(2).FirstOrDefault(), out size)) {
+                size = 100;
+            }
+            int margin;
+            if (!int.TryParse(qrSegments.Skip(3).FirstOrDefault(), out margin)) {
+                margin = 5;
+            }
+
+            //Get the target URL and use as text
+            string targetPath = url.Substring(0, url.IndexOf("/qrcode"));
+            string text = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/{targetPath}{HttpContext.Request.QueryString}";
 
             //Render the QR image
-            ZXing.Rendering.PixelData pixelData = QuickCore.Renderer.GetPixelData(text, size.Value, size.Value, margin.Value);
+            ZXing.Rendering.PixelData pixelData = QuickCore.Renderer.GetPixelData(text, size, size, margin);
 
             // creating a bitmap from the raw pixel data; if only black and white colors are used it makes no difference   
             // that the pixel data ist BGRA oriented and the bitmap is initialized with RGB   
             using (System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb))
-            using (MemoryStream ms = new MemoryStream())
-            {
+            using (MemoryStream ms = new MemoryStream()) {
                 QuickCore.Renderer.DrawBitmap(pixelData, bitmap);
 
                 bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
                 return File(ms.GetBuffer(), "image/png");
             }
         }
-
-        
-
     }
 }
